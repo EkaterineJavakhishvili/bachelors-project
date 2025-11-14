73576d0875a0a33dc8290a834034b04079b1f93c
Ekaterine Javakhishvili <javakhishvili.ekaterine@kiu.edu.ge>
2025-11-14 15:55:25 +0400

    update defaults and typo errors

--- src/config.py
+++ src/config.py
@@ -3,8 +3,6 @@ from datetime import date
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Values in .env file will be environmental variables (Anything becomes available via os.getenv)
-
 
 # Directories for the pipeline
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
@@ -16,16 +14,20 @@ MODELS_DIR = PROJECT_ROOT / "models" / "price_agent"
 FIG_DIR = PROJECT_ROOT / "reports" / "figures"
 
 
-# Double checking if the folders exist
+# Double checking if the folders exist (for bellow not to crush during saving)
 for d in [RAW_PRICES, INT_PRICES, PROC_PRICES, MODELS_DIR, FIG_DIR]:
     d.mkdir(parents=True, exist_ok=True)
+
+load_dotenv(
+    PROJECT_ROOT / ".env"
+)  # Values in .env file will be environmental variables (Anything in .env (keys) becomes available via os.getenv)
 
 
 #  If .env doesn't have values, use the defaults below
 TICKER = os.getenv("TICKER", "NFLX")  # Stock symbol we use
 START = os.getenv("PRICES_START", "2018-01-01")  # Date range for hist price data
 END = os.getenv("PRICES_END", str(date.today()))  # Date range for hist price data
-LOOKBACK = int(os.getenv("LOOCKBACK", "60"))  # How many past days LSTM looks to predict
+LOOKBACK = int(os.getenv("LOOKBACK", "60"))  # How many past days LSTM looks to predict
 TEST_RATIO = float(os.getenv("TEST_RATIO", "0.2"))  # 20% test set
 VAL_RATIO = float(
     os.getenv("VAL_RATIO", "0.125")
